cb82fb113fc1c98621c45417b31452ea441d12d0
Carlostq06 <carlosbarcelona280@gmail.com>
2026-07-02 12:33:28 +0200

    configuracion emaik

---
+++ .vercel/output/_functions/chunks/about_DgU0M9aJ.mjs
@@ -0,0 +1,69 @@
+import { t as __exportAll } from "./rolldown-runtime_D7D4PA-g.mjs";
+import { C as createComponent, a as renderComponent, d as renderTemplate, h as maybeRenderHead } from "./server_BoVuA2Pq.mjs";
+import { n as $$Image } from "./_astro_assets_qRTLEgEc.mjs";
+import { n as $$SiteHeader, r as $$Layout, t as $$SiteFooter } from "./SiteFooter_B_AvMRrV.mjs";
+//#region src/assets/Gemini_Generated_Image_g6oiatg6oiatg6oi.png
+var Gemini_Generated_Image_g6oiatg6oiatg6oi_default = new Proxy({
+	"src": "/_astro/Gemini_Generated_Image_g6oiatg6oiatg6oi.-o7l1tcc.png",
+	"width": 2048,
+	"height": 2048,
+	"format": "png"
+}, { get(target, name, receiver) {
+	if (name === "clone") return structuredClone(target);
+	if (name === "fsPath") return "C:/Users/domin/Desktop/landing-page-CuidaLink/src/assets/Gemini_Generated_Image_g6oiatg6oiatg6oi.png";
+	return target[name];
+} });
+//#endregion
+//#region src/components/about/HeroaboutSection.astro
+var $$HeroaboutSection = createComponent(($$result, $$props, $$slots) => {
+	return renderTemplate`${maybeRenderHead($$result)}<header class="mb-20 text-center max-w-4xl mx-auto"><span class="inline-block px-4 py-1.5 rounded-full bg-primary/90 text-on-primary-fixed text-xs text-white font-bold tracking-widest uppercase mb-6">Nuestra Esencia</span><h1 class="text-5xl md:text-6xl font-headline font-extrabold tracking-tight text-on-surface mb-8">Conoce a nuestro equipo</h1><p class="text-lg md:text-xl text-secondary font-body leading-relaxed">Creamos la herramienta que nosotros mismos usaríamos para cuidar a nuestros seres queridos. Más tranquilidad.</p></header><section class="px-6 grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-20 items-start"><div class="card-glint delay-3 hover-lift group flex flex-col items-center md:items-start p-8 rounded-xl border-slate-100 bg-white  shadow-sm  hover:border-primary/20 transition-all duration-500"><div class="relative mb-10"><div class="absolute inset-0 bg-primary/5 rounded-full scale-110 blur-xl group-hover:bg-primary/10 transition-colors"></div>${renderComponent($$result, "Image", $$Image, {
+		"alt": "CTO & Partner",
+		"class": "aspect-square w-55 h-55 object-cover rounded-full  border-4 border-surface-container-lowest shadow-xl  transition-all duration-500",
+		"src": Gemini_Generated_Image_g6oiatg6oiatg6oi_default,
+		"loading": "eager"
+	})}</div><div><h3 class="text-3xl font-headline font-bold text-on-surface mb-2">Carlos Tumbaco</h3><p class="text-primary font-bold text-label-md tracking-wider uppercase mb-6 flex items-center gap-2"><span class="material-symbols-outlined text-xl">verified_user</span> Founder &amp; CEO</p><p class="text-slate-600 text-lg leading-relaxed mb-8">Con más de 15 años de experiencia en gestión hospitalaria, Carlos fundó CuidaLink con la visión de democratizar el acceso a cuidados profesionales, asegurando que cada paciente reciba atención digna en la comodidad de su casa.</p></div><div class="flex gap-4"><button class="p-3 rounded-full text-slate-600  hover:text-primary hover:bg-primary-fixed transition-all"><span class="material-symbols-outlined">alternate_email</span></button><button class="p-3 rounded-full text-slate-600  hover:text-primary hover:bg-primary-fixed transition-all"><span class="material-symbols-outlined">link</span></button></div></div><div class="card-glint delay-3 hover-lift group flex flex-col md:mt-24 items-center md:items-start p-8 rounded-xl border-slate-100 bg-white  shadow-sm  hover:border-primary/20 transition-all duration-500"><div class="relative mb-10"><div class="absolute inset-0 bg-primary/5 rounded-full scale-110 blur-xl group-hover:bg-primary/10 transition-colors"></div>${renderComponent($$result, "Image", $$Image, {
+		"alt": "CTO & Partner",
+		"class": "aspect-square w-55 h-55 object-cover rounded-full border-4 border-surface-container-lowest shadow-xl  transition-all duration-500",
+		"src": Gemini_Generated_Image_g6oiatg6oiatg6oi_default,
+		"loading": "eager"
+	})}</div><div><h3 class="text-3xl font-headline font-bold text-on-surface mb-2">Alonso de la Peña</h3><p class="text-primary font-bold text-label-md tracking-wider uppercase mb-6 flex items-center gap-2"><span class="material-symbols-outlined text-xl">verified_user</span> CTO &amp; Partner</p><p class="text-slate-600 text-lg leading-relaxed mb-8">Con más de 15 años de experiencia en gestión hospitalaria, Alonso fundó CuidaLink con la visión de democratizar el acceso a cuidados profesionales, asegurando que cada paciente reciba atención digna en la comodidad de su casa.</p></div><div class="flex gap-4"><button class="p-3 rounded-full text-slate-600  hover:text-primary hover:bg-primary-fixed transition-all"><span class="material-symbols-outlined">alternate_email</span></button><button class="p-3 rounded-full text-slate-600  hover:text-primary hover:bg-primary-fixed transition-all"><span class="material-symbols-outlined">link</span></button></div></div></section>`;
+}, "C:/Users/domin/Desktop/landing-page-CuidaLink/src/components/about/HeroaboutSection.astro", void 0);
+//#endregion
+//#region src/assets/laptop.png
+var laptop_default = new Proxy({
+	"src": "/_astro/laptop.DYOJIE9x.png",
+	"width": 1472,
+	"height": 1472,
+	"format": "png"
+}, { get(target, name, receiver) {
+	if (name === "clone") return structuredClone(target);
+	if (name === "fsPath") return "C:/Users/domin/Desktop/landing-page-CuidaLink/src/assets/laptop.png";
+	return target[name];
+} });
+//#endregion
+//#region src/components/about/Ourpillars.astro
+var $$Ourpillars = createComponent(($$result, $$props, $$slots) => {
+	return renderTemplate`${maybeRenderHead($$result)}<section class="mt-20   py-20 md:px-6 "><h2 class="text-5xl font-sans font-bold text-center mb-16">Nuestros pilares fundamentales</h2><div class="grid grid-cols-1 md:grid-cols-3 gap-2 "><div class="hover:border-primary/20 hover:bg-primary-light/30 card-glint reveal reveal-up delay-3 hover-lift md:col-span-2  flex flex-col  md:items-start p-8 rounded-xl border-[#F2F4F7] bg-[#F2F4F7]  shadow-sm  hover:border-primary/20 transition-all duration-500"><div class="icon-pop hover:bg-primary-light/30 w-12 h-12 rounded-lg bg-primary/20 flex items-center justify-center text-primary mb-6"><span class="material-symbols-outlined">qr_code</span></div><h3 class="text-2xl font-bold text-on-surface mb-4">Conexión sin Fricción</h3><p class="text-slate-600 text-start leading-relaxed">Olvida las configuraciones complejas. Nuestra tecnología de vinculación por código QR permite conectar al cuidador con el paciente en segundos.</p></div><div class="card-glint reveal reveal-up delay-3 hover-lift md:col-span-1  flex flex-col  md:items-start p-8 rounded-xl border-primary bg-primary hover:bg-primary-hover shadow-sm  hover:border-primary transition-all duration-500"><div class="icon-pop hover:bg-primary-hover w-12 h-12 rounded-lg bg-primary-light/20 flex items-center justify-center text-primary-light mb-6"><span class="material-symbols-outlined">health_and_safety</span></div><h3 class="text-2xl text-white font-bold text-on-surface mb-4">Empatía Digital</h3><p class="text-white text-start leading-relaxed">Priorizamos la legibilidad y la simplicidad para que el monitoreo sea una ayuda.</p></div><div class="hover:border-primary/20 hover:bg-primary-light/30 card-glint reveal reveal-up delay-3 hover-lift md:col-span-1  flex flex-col  md:items-start p-8 rounded-xl border-[#F2F4F7] bg-[#F2F4F7]  shadow-sm  hover:border-primary/20 transition-all duration-500"><div class="icon-pop hover:bg-primary-light/30 w-12 h-12 rounded-lg bg-[#59c991] flex items-center justify-center text-[#002112] mb-6"><span class="material-symbols-outlined">add_alert</span></div><h3 class="text-2xl font-bold text-on-surface mb-4">Alertas Reales</h3><p class="text-slate-600 text-start leading-relaxed">Notificaciones S.O.S. inmediatas mediante WebSockets.</p></div><div class="hover:border-primary/20 hover:bg-primary-light/30 card-glint reveal reveal-up delay-3 hover-lift md:col-span-2 p-8 rounded-xl bg-surface-container-low flex items-center gap-8 border-[#F2F4F7] bg-[#F2F4F7]  shadow-sm  hover:border-primary/20 transition-all duration-500"><div class="icon-pop hover:bg-primary-light/30 hidden sm:block w-32 h-32 rounded-xl overflow-hidden shrink-0">${renderComponent($$result, "Image", $$Image, {
+		"alt": "Equipo trabajando",
+		"class": "w-full h-full object-cover",
+		"data-alt": "Hands of a diverse medical team stacked together",
+		"src": laptop_default
+	})}</div><div><h3 class="text-2xl font-bold text-on-surface mb-4">Privacidad Familiar</h3><p class="text-slate-600 text-start leading-relaxed">Tus datos son tuyos. Implementamos un modelo de acceso restringido donde solo los familiares invitados mediante PIN seguro pueden ver la ubicación y actividad del paciente.</p></div></div></div></section>`;
+}, "C:/Users/domin/Desktop/landing-page-CuidaLink/src/components/about/Ourpillars.astro", void 0);
+//#endregion
+//#region src/pages/about.astro
+var about_exports = /* @__PURE__ */ __exportAll({
+	default: () => $$About,
+	file: () => $$file,
+	url: () => $$url
+});
+var $$About = createComponent(($$result, $$props, $$slots) => {
+	return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Sobre Nosotros - CuidaLink" }, { "default": ($$result) => renderTemplate`${maybeRenderHead($$result)}<div class="relative w-full overflow-x-hidden">${renderComponent($$result, "SiteHeader", $$SiteHeader, {})}<main class="pt-25  max-w-7xl mx-auto">${renderComponent($$result, "HeroaboutSection", $$HeroaboutSection, {})}${renderComponent($$result, "Ourpillars", $$Ourpillars, {})}</main>${renderComponent($$result, "SiteFooter", $$SiteFooter, {})}</div>` })}`;
+}, "C:/Users/domin/Desktop/landing-page-CuidaLink/src/pages/about.astro", void 0);
+var $$file = "C:/Users/domin/Desktop/landing-page-CuidaLink/src/pages/about.astro";
+var $$url = "/about";
+//#endregion
+//#region \0virtual:astro:page:src/pages/about@_@astro
+var page = () => about_exports;
+//#endregion
+export { page };
